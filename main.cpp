cb27f38e346812159c724e4f0267f2a01534f03a
xsillls <rem240829@gmail.com>
2025-01-09 00:22:54 +0200

    1v

---
+++ main.cpp
@@ -0,0 +1,26 @@
+
+#include <iostream>
+#include <string>
+#include <windows.h>
+
+using namespace std;
+
+void ghoul()
+{
+	int namber = 993;
+
+	while (namber > 1)
+	{
+		cout << namber + 7 << "-7=" << namber << endl;
+		namber -= 7;
+		Sleep(50);
+	}
+}
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+	ghoul();
+
+	return 0;
+}
